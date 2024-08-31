d1e58004774099e4dbcf4775b8eddfb34b570f2f
vrravikant <ravikant1@valueresearch.in>
2024-08-31 12:06:22 +0530

    Some changes to main file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Welcome to Vite + React + Tailwind!</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Yugal is son of bitch :)</h1>
     </div>
   )
 }
